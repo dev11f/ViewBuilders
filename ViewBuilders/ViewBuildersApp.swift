8bebe0c2bbf8a922a206b3b3cfafdba8b9371174
kook <jumice10@gmail.com>
2022-11-29 11:57:48 +0900

    Initial Commit

---
+++ ViewBuilders/ViewBuildersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewBuildersApp.swift
+//  ViewBuilders
+//
+//  Created by kook on 2022/11/29.
+//
+
+import SwiftUI
+
+@main
+struct ViewBuildersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
